6136340eabc6243147875ccfc18fcff58fd74908
nik <nikhan@gmail.com>
2015-02-05 17:24:51 -0500

    test

--- server/compose.go
+++ server/compose.go
@@ -1,39 +1,62 @@
 package server
 
-import "github.com/nytlabs/st-core/core"
+import (
+	"fmt"
 
-func (p *Pattern) findComponents() ([]*Pattern, error) {
+	"github.com/nytlabs/st-core/core"
+)
 
-	return []*Pattern{}, nil
-}
+func (p *Pattern) components() ([]*Pattern, error) {
+	blocks := make([]BlockLedger, len(p.Blocks))
+	copy(blocks, p.Blocks)
+	components := []*Pattern{}
 
-func (p *Pattern) findLastNode() (*BlockLedger, error) {
+	var connected func(int) []int
+	connected = func(id int) []int {
+		ids := []int{}
+		for _, c := range p.Connections {
+			if c.Source.Id == id {
+				ids = append(ids, connected(c.Target.Id)...)
+			}
+			if c.Target.Id == id {
+				ids = append(ids, connected(c.Target.Id)...)
+			}
+		}
+		return ids
+	}
+	fmt.Println(blocks)
+	for len(blocks) > 0 {
+		cp := &Pattern{}
+		start, blocks := blocks[len(blocks)-1], blocks[:len(blocks)-1]
+		cp.Blocks = append(cp.Blocks, start)
 
-	return &BlockLedger{}, nil
-}
-
-func (p *Pattern) tail(b *BlockLedger) []*BlockLedger {
-
-	return []*BlockLedger{}
-}
-
-func (p *Pattern) head() (*BlockLedger, error) {
-	return &BlockLedger{}, nil
+		ids := connected(start.Id)
+		newBlocks := []BlockLedger{}
+		for _, b := range blocks {
+			remove := false
+			for _, id := range ids {
+				if id == b.Id {
+					cp.Blocks = append(cp.Blocks, b)
+					remove = true
+				}
+			}
+			if remove {
+				fmt.Println("REMOVED")
+				continue
+			}
+			newBlocks = append(newBlocks, b)
+		}
+		blocks = newBlocks
+		components = append(components, cp)
+	}
+	return components, nil
 }
 
 func (p *Pattern) Spec() (*core.Spec, error) {
 
-	patterns, err := p.findComponents()
+	_, err := p.components()
 	if err != nil {
 		return nil, err
-	}
-
-	for i, p := range patterns {
-		head, err := p.head()
-		if err != nil {
-			return nil, err
-		}
-		_ = head
 	}
 
 	return nil, nil
---
+++ server/compose_test.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+type PatternTest struct {
+	PatternInput       string
+	ExpectedComponents int
+}
+
+var tests = []PatternTest{
+	PatternTest{
+		PatternInput:       `{"blocks":[{"label":"","type":"+","id":1,"inputs":[{"name":"addend","type":"fetch","value":"."},{"name":"addend","type":"fetch","value":"."}],"outputs":[{"name":"sum"}],"position":{"x":0,"y":0}},{"label":"","type":"+","id":2,"inputs":[{"name":"addend","type":"fetch","value":"."},{"name":"addend","type":"fetch","value":"."}],"outputs":[{"name":"sum"}],"position":{"x":0,"y":0}},{"label":"","type":"+","id":3,"inputs":[{"name":"addend","type":"fetch","value":"."},{"name":"addend","type":"fetch","value":"."}],"outputs":[{"name":"sum"}],"position":{"x":0,"y":0}}],"connections":[{"source":{"id":2,"route":0},"target":{"id":1,"route":0},"id":4},{"source":{"id":3,"route":0},"target":{"id":1,"route":0},"id":5}],"groups":[{"id":0,"label":"root","children":[1,2,3],"position":{"x":0,"y":0}}],"sources":null,"links":null}`,
+		ExpectedComponents: 1,
+	},
+	PatternTest{
+		PatternInput:       `{"blocks":[{"label":"","type":"+","id":1,"inputs":[{"name":"addend","type":"fetch","value":"."},{"name":"addend","type":"fetch","value":"."}],"outputs":[{"name":"sum"}],"position":{"x":0,"y":0}},{"label":"","type":"+","id":2,"inputs":[{"name":"addend","type":"fetch","value":"."},{"name":"addend","type":"fetch","value":"."}],"outputs":[{"name":"sum"}],"position":{"x":0,"y":0}},{"label":"","type":"+","id":3,"inputs":[{"name":"addend","type":"fetch","value":"."},{"name":"addend","type":"fetch","value":"."}],"outputs":[{"name":"sum"}],"position":{"x":0,"y":0}},{"label":"","type":"+","id":6,"inputs":[{"name":"addend","type":"fetch","value":"."},{"name":"addend","type":"fetch","value":"."}],"outputs":[{"name":"sum"}],"position":{"x":0,"y":0}}],"connections":[{"source":{"id":2,"route":0},"target":{"id":1,"route":0},"id":4},{"source":{"id":3,"route":0},"target":{"id":1,"route":0},"id":5}],"groups":[{"id":0,"label":"root","children":[1,2,3,6],"position":{"x":0,"y":0}}],"sources":null,"links":null}`,
+		ExpectedComponents: 2,
+	},
+	PatternTest{
+		PatternInput:       `{"blocks":[{"label":"","type":"+","id":1,"inputs":[{"name":"addend","type":"fetch","value":"."},{"name":"addend","type":"fetch","value":"."}],"outputs":[{"name":"sum"}],"position":{"x":0,"y":0}},{"label":"","type":"+","id":2,"inputs":[{"name":"addend","type":"fetch","value":"."},{"name":"addend","type":"fetch","value":"."}],"outputs":[{"name":"sum"}],"position":{"x":0,"y":0}},{"label":"","type":"+","id":3,"inputs":[{"name":"addend","type":"fetch","value":"."},{"name":"addend","type":"fetch","value":"."}],"outputs":[{"name":"sum"}],"position":{"x":0,"y":0}},{"label":"","type":"+","id":6,"inputs":[{"name":"addend","type":"fetch","value":"."},{"name":"addend","type":"fetch","value":"."}],"outputs":[{"name":"sum"}],"position":{"x":0,"y":0}},{"label":"","type":"+","id":7,"inputs":[{"name":"addend","type":"fetch","value":"."},{"name":"addend","type":"fetch","value":"."}],"outputs":[{"name":"sum"}],"position":{"x":0,"y":0}}],"connections":[{"source":{"id":2,"route":0},"target":{"id":1,"route":0},"id":4},{"source":{"id":3,"route":0},"target":{"id":1,"route":0},"id":5}],"groups":[{"id":0,"label":"root","children":[1,2,3,6,7],"position":{"x":0,"y":0}}],"sources":null,"links":null}`,
+		ExpectedComponents: 3,
+	},
+}
+
+func TestPatternComponents(t *testing.T) {
+	for _, p := range tests {
+		var po Pattern
+		err := json.Unmarshal([]byte(p.PatternInput), &po)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(po.components())
+	}
+}
